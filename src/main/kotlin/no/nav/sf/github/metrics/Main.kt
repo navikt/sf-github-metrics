4d4719bd64d25a189948a6f6223e0d2eb890b937
nocturnalnematode <106680091+nocturnalnematode@users.noreply.github.com>
2025-05-12 14:00:48 +0200

    also allow calls without instance

--- src/main/kotlin/no/nav/sf/github/metrics/Main.kt
+++ src/main/kotlin/no/nav/sf/github/metrics/Main.kt
@@ -17,26 +17,41 @@ import io.ktor.util.reflect.typeInfo
 
 val logger: Logger = LoggerFactory.getLogger("Main")
 
+/**
+ * forward stats for a given job and an optionally specified instance
+ * often the instance is given in the data instead of the path
+ */
+suspend fun forward(body: String?, jobname: String, instance: String? = null): HttpResponse {
+    val client = HttpClient(CIO)
+    val root = System.getenv("PROMGATEADDRESS") ?: "http://172.17.0.3:9091" // default to private network https://www.rfc-editor.org/rfc/rfc1918
+    val url = instance?.let { "${root}/metrics/job/$jobname/instance/$it" } ?: "${root}/metrics/job/$jobname"
+
+    val response: HttpResponse = client.post(url) {
+        contentType(ContentType.Text.Plain)
+        setBody(body)
+    }
+    return response
+}
+
 fun Application.module() {
     routing {
         route("/metrics/job/{jobname}/instance/{instance}") {
             post {
                 val body: String? = call.receiveNullable(typeInfo<String>())
-                val jobname = call.parameters.get("jobname")
-                val instance = call.parameters.get("instance")
-                val client = HttpClient(CIO)
-                val response: HttpResponse = client.post(System.getenv("PROMGATEADDRESS") ?: "http://172.17.0.3:9091/metrics/job/$jobname/instance/$instance") {
-                    contentType(ContentType.Text.Plain)
-                    setBody(body)
-                }
+                val jobname = call.parameters.get("jobname")!!
+                val instance = call.parameters.get("instance")!!
+                val response = forward(body, jobname, instance)
                 logger.info("${response}")
                 call.respond("whatever\n")
             }
         }
         route("/metrics/job/{jobname}") {
             post {
-                logger.info("${call.parameters.getAll("jobname")} got")
-                call.respond(HttpStatusCode.OK, "something's ok!\n")
+                val body: String? = call.receiveNullable(typeInfo<String>())
+                val jobname = call.parameters.get("jobname")!!
+                val response = forward(body, jobname)
+                logger.info("${response}")
+                call.respond("whomever\n")
             }
         }
     }
