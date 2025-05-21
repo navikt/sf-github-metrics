104f7d038bab94c3fb0f1d36c40016b562bce4e5
nocturnalnematode <106680091+nocturnalnematode@users.noreply.github.com>
2025-05-22 00:39:11 +0200

    add another key for testing

--- src/main/kotlin/no/nav/sf/github/metrics/Runners.kt
+++ src/main/kotlin/no/nav/sf/github/metrics/Runners.kt
@@ -7,6 +7,7 @@ class Runners {
     companion object {
         val publicKeys = mapOf(
             "local" to "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEFkN2IQqTBAubNkBZV15vEzaRfC9+DDg5r2fu/xSMnVqmjjNX0w5wowRLrYgtmPsf0AveQCqXkzuqXbeDvSWFng==",
+            "utah" to "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExWiWCHrqEcsEvXxEIlnyEt2GkqOIruU0f62D4HqO5T7JXQMvYlNd/aoHln4wpV1+YCpvqWxGS1sYmTEvB1w3aw==",
         )
     }
 }
