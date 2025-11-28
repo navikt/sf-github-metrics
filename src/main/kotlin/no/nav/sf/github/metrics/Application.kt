a07ab4160cca418615429e832e04895b7a83afda
Björn Hägglund <bjorn.hagglund@nav.no>
2025-11-28 14:05:49 +0100

    refresh gui on start

--- src/main/kotlin/no/nav/sf/github/metrics/Application.kt
+++ src/main/kotlin/no/nav/sf/github/metrics/Application.kt
@@ -301,7 +301,11 @@ class Application {
                     <div style='margin-bottom:16px;'>
                         <div id='recordLamp' class='status-lamp'></div>
                         <button class='control-btn start-btn' onclick="fetch('/internal/start')">Start</button>
-                        <button class='control-btn stop-btn'  onclick="fetch('/internal/stop')">Stop</button>
+                        <button class='control-btn stop-btn'  onclick="
+                            fetch('/internal/start').then(() => {
+                                location.reload();
+                             });
+                            "">Stop</button>
                         <button class='control-btn clear-btn' onclick="fetch('/internal/clear')">Clear</button>
                     </div>
                     """.trimIndent(),
